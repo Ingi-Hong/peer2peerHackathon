fbe7dc7bb48e9a81e165bca09db092e0a8425514
Tristen Liu <tristenliu9876@gmail.com>
2023-03-30 10:03:03 -0400

    Combined client and server

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ import socket
 import threading
 
 #TODO:end connection with 'exit'
-def connect(s):
+def client_connect(s):
     while True:
         r_msg = s.recv(1024)
         if not r_msg:
@@ -15,7 +15,7 @@ def connect(s):
         else:
             print(r_msg.decode())
 
-def receive(s):
+def client_receive(s):
     while True:
         s_msg = input().replace('b', '').encode('utf-8')
         if s_msg == '':
@@ -26,17 +26,51 @@ def receive(s):
         else:
             s.sendall(s_msg)
 
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("usage: %s [ip adress][port] " % sys.argv[0] )
-        sys.exit(0)
+
+def server_connect(conn):
+    while True:
+        received = conn.recv(1024)
+        if received ==' ':
+            pass
+        else:
+            print(received.decode())
+
+def server_sendMsg(conn):
+    while True:
+        send_msg = input().replace('b', '').encode()
+        if send_msg == ' ':
+            pass
+        else:
+            conn.sendall(send_msg)
+
+
+def init_client(host, port):
+    print("usage: %s [ip adress][port] " % host)
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.connect((sys.argv[1], int(sys.argv[2])))
-    thread1 = threading.Thread(target = connect, args = ([s]))
-    thread2 = threading.Thread(target = receive, args = ([s]))
+    thread1 = threading.Thread(target = client_connect, args = ([s]))
+    thread2 = threading.Thread(target = client_receive, args = ([s]))
     thread1.start()
     thread2.start()
     thread1.join()
     thread2.join()
+
+
+def init_server(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('172.20.10.2', 49152))
+    s.listen()
+    (conn, addr) = s.accept() 
+    thread1 = threading.Thread(target = server_connect, args = ([conn]))
+    thread2 = threading.Thread(target = server_sendMsg, args = ([conn]))
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
+
+if __name__ == '__main__':
+    
+    thread_c = threading.Thread(target=init_client, args = ())
